310e35968e6c4cc7aaa304fe690f697853151db3
daffahilmyf <daffahilmyf@student.telkomuniversity.ac.id>
2021-02-04 00:54:39 +0700

    testing on unit in login service

--- Server/Login/routes/sign-in.js
+++ Server/Login/routes/sign-in.js
@@ -7,59 +7,94 @@ const bcrypt = require('bcryptjs')
 const db = require('../model/user.js');
 
 
-route.get('/user', async(req, res)=>{
-    await db.find({}, (err, user)=>{
-        if(err){
-            res.status(404)
-                .send('Sorry, we cannot find that');;
-        }
+async function userValidatePassword(user, password, res){
+	try {
 
-        return res.status(200).json({ data: user})
-    })
+		const isMatch = await bcrypt.compare(password, user.password);
+		console.log(isMatch)
+
+		switch (isMatch) {
+			case true:
+				return res.status(200).json({
+					status: 'Success',
+					data: user,
+				})
+		
+			default:
+				return res.status(400).json({
+					status: 'error',
+					error: 'Password incorrect',
+				});
+		}
+
+
+
+		// bcrypt.compare(password, user.password, (isMatch) =>{
+		// 	if (isMatch) {
+		// 		return res.status(200).json({
+		// 			status: 'Success',
+		// 			data: data,
+		// 		})
+		// 	} else {
+		// 		
+		// 	}
+		// })
+	} catch (error) {
+		console.error(error)
+	}
+	
+}
+
+
+// TODO check kenapa kgk bisa dihandle ama case su
+function userAccountCheck(user, password, res){
+	try {
+		if(!user){
+			return res.status(400).json({
+					status: 'error',
+					error: 'NIM is not registered or cannot be empty',
+			});
+		}
+		if(user){
+			userValidatePassword(user, password, res);
+		}
+		// switch (user !== null) {
+		// 	case false:
+		// 		userValidatePassword(user, password, res);
+		// 	default:
+		// 		return res.status(400).json({
+		// 			status: 'error',
+		// 			error: 'NIM is not registered or cannot be empty',
+		// 		}); 
+		// }
+	} catch (error) {
+		console.error(error)
+	}
+}
+
+
+function userCheckAndValidate(email, password, res){
+	try {
+		db.findOne({email:email}).then((user)=>{
+			userAccountCheck(user, password, res);
+		})
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+
+//POST
+route.post('/login', async (req, res) => {
+	try {
+		const {email, password} = req.body;
+		await userCheckAndValidate(email, password, res);
+	} catch (err) {
+		console.error(err)
+	}
 })
 
-//TODO: add middleware for auth
-// IS: user input nim & password
-// FS: user login success
-route.post('/login', (req, res) =>{
 
-    const { nim, password } = req.body;
-
-    db.findOne({nim: nim}).then((err, user)=>{
-        if (err) return err;
-
-        if(!user){
-            return res.status(400).json({
-				status: 'error',
-				error: 'email is not registered or cannot be empty',
-			}); 
-        }
-
-        if(user){
-            const data = {
-				userId : user.id, 
-				roles : user.roles, 
-				name : user.name,
-			};
-			
-			bcrypt.compare(password, user.password, (err, isMatch) =>{
-				if (isMatch) {
-					res.status(200).json({
-						status: 'Success',
-						data: data,
-					})
-					console.log(data);
-					
-				} else {
-					return res.status(400).json({
-						status: 'error',
-						error: 'Password incorrect',
-					});
-				}
-			})
-        }
-    })
-})
 
 module.exports = route;
 
--- Server/Login/routes/sign-up.js
+++ Server/Login/routes/sign-up.js
@@ -9,7 +9,7 @@ const db = require('../model/user.js');
 
 
 route.post('/register', async(req, res)=> {
-    const { name, email, password} = req.body;
+    const { name, email, nim, password} = req.body;
 
     await db.findOne({email: email})
       .then((user)=>{
@@ -22,6 +22,7 @@ route.post('/register', async(req, res)=> {
 			const newUser = new db({
 				name,
 				email,
+				nim,
 				password,
 			});
 
