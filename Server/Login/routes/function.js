3bf2247b04a5533f72d5c7957afc74487ed7eca3
daffahilmyf <daffahilmyf@student.telkomuniversity.ac.id>
2021-02-04 12:53:55 +0700

    add some code

--- Server/Login/routes/function.js
+++ Server/Login/routes/function.js
@@ -17,10 +17,10 @@ const db = require('../model/user.js');
 // TODO: make it more simple & what json the FE needs to return
 // IS: comparing the user password with the hased password in the database
 // FS: Validated or Not
-async function userValidatePassword(user, password, res){
+function userValidatePassword(user, password, res){
 	try {
 
-		const isMatch = await bcrypt.compare(password, user.password);
+		const isMatch = bcrypt.compareSync(password, user.password);
 		switch (isMatch) {
 			case true:
 				return res.status(200).json({
@@ -94,11 +94,12 @@ async function userCheckAndValidate(nim, password, res){
 // }
 
 
-function userNIMCheck(nim){
+function userNIMInputCheck(nim){
     if(nim == null){
 
         return false;
     }
+    
 	if(nim.toString().length == 10){
 		return true;
 	}else{
@@ -107,7 +108,7 @@ function userNIMCheck(nim){
 }
 
 
-function userPasswordCheck(password){
+function userPasswordInputCheck(password){
 	if(password == null){
 		return false;
 	}else{
@@ -116,23 +117,12 @@ function userPasswordCheck(password){
 }
 
 async function userInputCheck(nim, password){
-    const isNIMTrue = await userNIMCheck(nim);
-    const isPassTrue = await userPasswordCheck(password);
+    const isNIMTrue = await userNIMInputCheck(nim);
+    const isPassTrue = await userPasswordInputCheck(password);
     return isPassTrue === isNIMTrue
 }
 
-// async function userCreateAccount(name, email, nim, password){
 
-// 	const status = {
-// 		email: await userEmailCheck(email),
-// 		nim: await userNIMCheck(nim),
-// 		password: await userPasswordCheck(password),
-// 	}
-
-// 	if(status.email && status.nim && status.password === true){
-
-// 	}
-// }
 
 module.exports = {
     userAccountCheck, 
